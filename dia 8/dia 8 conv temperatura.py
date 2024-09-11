4b2c6ccab3244085de33bc596535d96ce344cbd8
ipasouza99 <ipasouza99@gmail.com>
2024-09-11 19:20:03 -0300

    dia 8

---
+++ dia 8/dia 8 conv temperatura.py
@@ -0,0 +1,12 @@
+def c_p_f(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+entrada = float(input("Digite a temperatura em Celsius °C:   " ))
+
+print(f"A temperatura em  Fahrenheit é:  {c_p_f(entrada)} °F") 
+
+
+
+
+
