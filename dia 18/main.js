d967e545134dbc1551830124955cd54c4bb6cc96
ipasouza99 <ipasouza99@gmail.com>
2024-09-23 18:04:37 -0300

    dia 18 Jogo da Velha

---
+++ dia 18/main.js
@@ -0,0 +1,60 @@
+let bloco = document.querySelectorAll("td");
+let msg = document.querySelector('p');
+let reset = document.querySelector('button');
+let jogada = "O";
+
+bloco.forEach(casa => {
+    casa.addEventListener('click', event => {
+        if (casa.innerHTML == "") {
+            casa.innerText = jogada;
+            jogada = jogada == "X" ? "O" : "X";
+            msg.innerText = `Próximo a jogar: ${jogada}`;
+            verificaGanhador();
+        }
+    })
+})
+
+reset.addEventListener('click', event => {
+    bloco.forEach(casa =>{
+        casa.innerText = "";
+        msg.innerText = `Próximo a jogar: ${jogada}`;
+    })
+} )
+
+function verificaGanhador() {
+
+    let empate = true;
+    
+    if(
+        (bloco[0].innerText != "" && bloco[0].innerText == bloco[1].innerText && bloco[0].innerText == bloco[2].innerText) ||
+        (bloco[3].innerText != "" && bloco[3].innerText == bloco[4].innerText && bloco[3].innerText == bloco[5].innerText) ||
+        (bloco[6].innerText != "" && bloco[6].innerText == bloco[7].innerText && bloco[6].innerText == bloco[8].innerText) ||
+        (bloco[0].innerText != "" && bloco[0].innerText == bloco[1].innerText && bloco[0].innerText == bloco[2].innerText) ||
+        (bloco[0].innerText != "" && bloco[0].innerText == bloco[3].innerText && bloco[0].innerText == bloco[6].innerText) ||
+        (bloco[1].innerText != "" && bloco[1].innerText == bloco[4].innerText && bloco[1].innerText == bloco[7].innerText) ||
+        (bloco[2].innerText != "" && bloco[2].innerText == bloco[5].innerText && bloco[2].innerText == bloco[8].innerText) ||
+        (bloco[0].innerText != "" && bloco[0].innerText == bloco[4].innerText && bloco[0].innerText == bloco[8].innerText) ||
+        (bloco[2].innerText != "" && bloco[2].innerText == bloco[4].innerText && bloco[2].innerText == bloco[6].innerText) 
+    ) 
+    
+    {
+        msg.innerHTML = "<h1>Vitória!</h1>";
+        empate = false;
+    }
+
+    if(empate) {
+        let todasPreenchidas = true;
+        for(let i =0; i<bloco.length; i++){
+            if(bloco[i].innerText == "")
+            {
+                todasPreenchidas =false;
+                break;
+            }
+        }
+        if(todasPreenchidas)
+        {
+            msg.innerHTML = "<h1>Deu velha!</h1>";
+        }
+    }   
+
+} 
